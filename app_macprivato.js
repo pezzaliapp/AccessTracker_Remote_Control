9e04779c6065f16a2d7dd87884c67b90139be139
pezzaliapp <pezzalialessandro@gmail.com>
2025-04-07 22:14:30 +0200

    Add files via upload

---
+++ app_macprivato.js
@@ -0,0 +1,27 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCm9I1f2I8FQHiIUoSbtOmLRQNxtgCJd60",
+  authDomain: "accesstracker-5d3f9.firebaseapp.com",
+  projectId: "accesstracker-5d3f9",
+  storageBucket: "accesstracker-5d3f9.appspot.com",
+  messagingSenderId: "331964316032",
+  appId: "1:331964316032:web:8fcc4efdc180a40201a965"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+const dispositivo = "MacPrivato";
+
+db.collection("controllo").doc("stato").get().then(doc => {
+  if (doc.exists && doc.data().attivo) {
+    db.collection("accessi").add({
+      timestamp: new Date().toISOString(),
+      userAgent: navigator.userAgent,
+      dispositivo: dispositivo
+    }).then(() => {
+      console.log("Accesso registrato da " + dispositivo);
+    });
+  } else {
+    console.log("Tracker disattivato da remoto.");
+  }
+});
